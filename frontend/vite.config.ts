d49e8471424c8d749dd0089c7e9654c66db64a3f
Sindre Thorbjørnsen <sindre.th@gmail.com>
2024-11-17 11:02:15 +0100

    frontend packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/stena/',
+  server: {
+    open: true,
+    port: 3000
+  },
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
